a7128cc284964d1b274f2f61f0e7eb1924317e01
Nicola Solo <solo.nicola@live.com>
2020-04-10 02:59:53 +0300

    a

---
+++ .history/js/bitesize_20200323161950.js
@@ -0,0 +1,32 @@
+let products = document.querySelector("#pro")
+$(function() {
+    function showData(arr) 
+      {
+        let out = "";
+        for(let i = 0; i < arr.length; i++) 
+          {
+            out += 
+            ' ( <span> ' + arr[i].id + ' </span> )  ' +
+            '<h4 class="title">' + arr[i].title + '</h4>' +
+            '<p>' + arr[i].body + '</p>';
+          }
+        document.getElementById("products").innerHTML = out;
+      }
+  var xmlhttp = new XMLHttpRequest();
+  var url = "https://jsonplaceholder.typicode.com/posts";
+  var itemsPerPage = 8; // count of recors per page
+  var numPage = window.location.search.substring(6);
+  xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      var items = JSON.parse(this.responseText);
+      var subArrays = [];//arrays for pages
+      for (let i = 0; i < Math.ceil(items.length/itemsPerPage); i++){
+        subArrays[i] = items.slice((i*itemsPerPage), (i*itemsPerPage) + itemsPerPage);
+        $('<a>', { href: '?page='+(i+1)+'', text: ''+(i+1)+''}).appendTo('#pagination');
+      }
+      showData(subArrays[numPage-1]);
+    };
+  };
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send(); 
+  });
